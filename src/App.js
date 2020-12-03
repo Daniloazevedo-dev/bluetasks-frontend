e5803bbbcf24a5b0f54f6a583899418d51318fd8
Danilo <Danilo.ann@gmail.com>
2020-12-03 10:05:03 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import TaskForm from './components/TaskForm';
+import TaskLisTable from './components/TaskLisTable';
+import Login from './components/Login';
+import { AuthContext, useAuth } from './hooks/useAuth';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (props) => {
+
+  const auth = useAuth();
+
+    return (
+      <AuthContext.Provider value={auth} >
+        <BrowserRouter>
+          <div className="App">
+            <NavBar />
+            <div className="container" style={{ marginTop: 20 }}>            
+              <Switch>
+                <Route exact path="/login" component={Login}  />
+                <Route exact path="/form" component={TaskForm} />
+                <Route exact path="/form/:id" component={TaskForm} />
+                <Route path="/" component={TaskLisTable} />
+              </Switch>            
+            </div>
+          </div>
+        </BrowserRouter>
+      </AuthContext.Provider>
+    );
+  }
 
 export default App;
